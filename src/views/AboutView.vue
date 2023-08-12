5a023de0a72d1dff77d6550cc5f91eb8b67f8456
saber-seerat <saber.seerat66@gmail.com>
2023-08-12 10:52:38 +0430

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <main class="about-page">
+     <h1>About</h1>
+    <p>this is the about page</p>
+  </main>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <main class="home-page">
+     <h1>Home</h1>
+    <p>this is the home page</p>
+  </main>
+</template>
